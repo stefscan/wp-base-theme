398c223914cb48db58888c498c4ddb5fb21e8133
stefscan <stephcharette@gmail.com>
2023-12-04 13:13:22 -0500

    Add basic loop to index.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php get_header(); ?>
 
-    <p>Site content!</p>
+<?php if(have_posts()): ?>
+
+    <?php while(have_posts()): the_post(); ?>
+
+    <article>
+        <h2 class="title"><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+    </article>
+
+    <?php endwhile; ?>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
