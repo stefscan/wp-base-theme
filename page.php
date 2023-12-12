f8b527e2640116991e8c60d15c65901c0b0590c7
stefscan <stephcharette@gmail.com>
2023-12-12 13:39:57 -0500

    add page.php

---
+++ page.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+page.php
+
+<section>
+
+    <article>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+    </article>
+
+</section>
+
+<?php get_footer(); ?>
+
+
+
