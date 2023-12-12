b253797427ad5b12a5af8081afc12f515d2e9aa0
stefscan <stephcharette@gmail.com>
2023-12-12 13:38:58 -0500

    add page.php add show thumbnail in single

--- single.php
+++ single.php
@@ -3,9 +3,17 @@
 
 <section>
 
-    <article>
-        <h2><?php the_title(); ?></h2>
-        <p><?php the_content(); ?></p>
+    <article class="flex">
+        <div class="item-50">
+            <h2><?php the_title(); ?></h2>
+            <p><?php the_content(); ?></p>
+        </div>
+        <div class="item-50">
+        <?php if(has_post_thumbnail()) : ?>
+            <?php the_post_thumbnail('medium', ['class' => 'fluid']);  ?>
+        <?php endif; ?>
+        </div>
+        
     </article>
 
 </section>
