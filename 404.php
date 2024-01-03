9fc6a2bec814046ac74e093eac415321c4750071
stefscan <stephcharette@gmail.com>
2024-01-03 09:48:30 -0500

    feat add home link to 404 page

--- 404.php
+++ 404.php
@@ -3,6 +3,8 @@
 <article>
     <h2>404</h2>
     <p>Désolé... Il n'y a rien ici.</p>
+    <a href="<?php echo esc_url( home_url('/') ); ?>">
+                Revenir à la page d'accueil</a>
 </article>
 
 <?php get_footer( ); ?>
