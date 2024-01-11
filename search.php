eb259776a119697987e78ee77f1484a1df4354b6
stefscan <stephcharette@gmail.com>
2024-01-11 08:09:01 -0500

    fix add a section to search template

--- search.php
+++ search.php
@@ -1,7 +1,11 @@
 <?php get_header(); ?>
 
-<h2>Résultats de recherche : </h2>
+<section>
+
+    <h2>Résultats de recherche : </h2>
 
     <?php  get_template_part("templates/main-loop");  ?>
+
+</section>
 
 <?php get_footer(); ?>
